e883700b520104cf2bb420a7eb7b64fc8974e199
M Hasnain <mhasnain306@gmail.com>
2023-09-08 11:06:20 +0500

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+const apiClient = axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "a3512e9055a64e759bda14af7d0f371c"
+    }
+})
+
+export default apiClient;
